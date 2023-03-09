b00b18b6a35b2bfe6f511de0a869059cac72cf2d
ajavaguy <ajavaguy96@gmail.com>
2023-03-09 22:58:28 +0700

    Add SecurityConfile file for gateway

--- order-service/src/main/java/com/microservices/orderservice/advice/ExceptionService.java
+++ order-service/src/main/java/com/microservices/orderservice/advice/ExceptionService.java
@@ -3,14 +3,17 @@ package com.microservices.orderservice.advice;
 import com.microservices.orderservice.exception.OutOfStockException;
 import com.microservices.orderservice.model.ErrorObject;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionService {
 
+    @ResponseBody
     @ExceptionHandler(OutOfStockException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
     public ErrorObject outOfStockException(OutOfStockException outOfStockException) {
         return new ErrorObject(OutOfStockException.class.getSimpleName(), outOfStockException.getMessage());
     }
